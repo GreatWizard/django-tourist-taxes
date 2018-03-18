0796ad2b321f51f54da44adba57f768a997aa6d9
Guillaume Gérard <guillaume.gerard88@gmail.com>
2018-03-18 20:37:48 +0100

    better admin tables

--- touristtaxes/stays/admin.py
+++ touristtaxes/stays/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Location, Resident, Stay
 
-admin.site.register(Location)
-admin.site.register(Resident)
-admin.site.register(Stay)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class ResidentAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'location',
+        'date_begin',
+        'date_end',
+    )
+
+
+class StayAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'location',
+        'date_begin',
+        'date_end',
+        'adults',
+        'children',
+        'is_validated'
+    )
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Resident, ResidentAdmin)
+admin.site.register(Stay, StayAdmin)
